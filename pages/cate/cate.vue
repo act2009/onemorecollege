940b3ef9f71532157486c55848701ac6be04f0ed
Administrator <lalalacp3@sina.com>
2020-03-02 16:08:56 +0800

    增加分类模块

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,20 +1,41 @@
 <template>
-	<view>
-		分类页面
-	</view>
+    <view>
+        <category :categoryList="categoryList" :subCategoryList="subCategoryList" @categoryMainClick="categoryMainClick" @categorySubClick="categorySubClick"></category>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+	import category from '@/components/qiyue-category/qiyue-category.vue'
+        export default {
+            components:{
+                category
+            },
+            data() {
+                return {
+                    categoryList:[],
+                    subCategoryList:[]
+                }
+            },
+            methods: {
+                categoryMainClick(category){
+                    this.subCategoryList = category.subCategoryList;
+                },
+                categorySubClick(category){
+                    console.log(category);
+                }
+            },
+            mounted() {
+                for(var i=0;i<20;i++){
+                    var subList = [];
+                    for(var j=0;j<30;j++){
+                        subList.push({"name":"分类"+i+":商品"+j,"logo":"http://placehold.it/50x50"})
+                    }
+                    this.categoryList.push({"name":"分类"+i,"subCategoryList":subList})
+                }
+                this.subCategoryList = this.categoryList[0].subCategoryList;
+            }
+        }
+    
 </script>
 
 <style>
